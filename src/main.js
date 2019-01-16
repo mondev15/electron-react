25b40fea052dd04c30b6db2f1dc1a7ded5aac7f0
mondev15 <evergiste.mutoni@atos.net>
2019-01-16 15:44:41 +0100

    adding renderer.js file

--- src/main.js
+++ src/main.js
@@ -16,8 +16,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
     // --- to hide menubar
     // eslint-disable-next-line no-trailing-spaces
-
-    // mainWindow.setMenu(null)
+    // mainWindow.setMenu(null) 
     mainWindow.loadURL(
         process.env.ELECTRON_START_URL
         || url.format({ pathname: path.join(__dirname, '/../build/index.html'), protocol: 'file:', slashes: true }),
